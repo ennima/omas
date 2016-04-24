1b0a0e4e6df623e69e361bbececcfcb084cb299e
Ennima <ennima@hotmail.com>
2016-04-23 22:26:46 -0500

    comments

--- omas.py
+++ omas.py
@@ -11,6 +11,7 @@ omas_project = "empty"
 print(lines)
 count = 0
 error = OmasError()
+activeScope = ""
 for line in lines:
 	print(line)
 	#first line
@@ -20,11 +21,21 @@ for line in lines:
 			lineSplit = line.split(' ')
 			omas_project = lineSplit[1]
 			print("PROJECT: ",omas_project)
+			activeScope = "project"
 			if(len(lineSplit)>2):
 				#print("#ERROR: El proyecto solo puede tener un nombre")
-				error.add("#ProjectError:","El proyecto solo puede tener un nombre",count+1)
+				print(error.add("#ProjectError:","El proyecto solo puede tener un nombre",count+1))
 		else:
 			#print("#ERROR: El script debe indicar a que proyecto pertenece.")
-			error.add("#ProjectError:","El script debe indicar a que proyecto pertenece.",count+1)
+			print(error.add("#ProjectError:","El script debe indicar a que proyecto pertenece.",count+1))
 		count+=1
-error.show()
+	else:
+		#Comment
+		if(line.startswith("#")):
+			print("Comment: ",line.strip('#'))
+		elif("#" in line):
+			print("posible comment")
+			if("\t" in line):
+				print("fue tabulado")
+		count+=1
+print(error.show())
