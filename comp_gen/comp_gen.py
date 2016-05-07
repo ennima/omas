087de2ca59bf106a20e6f982d960e3c18532b636
enrique nieto  martinez <enrique nieto  martinez>
2016-05-07 14:05:24 -0500

    CompGen

---
+++ comp_gen/comp_gen.py
@@ -0,0 +1,11 @@
+import os, sys, datetime, json
+
+#projectName = input("Nombre del Projecto: ")
+
+projectName = "test"
+author={
+            "name": "Enrique Nieto",
+            "email": "ennima@hotmail.com"
+        }
+
+print(projectName," ---- ")
