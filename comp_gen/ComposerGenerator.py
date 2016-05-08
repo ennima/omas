746dff58958bfe7857da89cd251957fc3b7d7b45
Ennima <ennima@hotmail.com>
2016-05-08 00:36:42 -0500

    ComposerGenTest

--- comp_gen/ComposerGenerator.py
+++ comp_gen/ComposerGenerator.py
@@ -22,9 +22,108 @@ class ComposerGenerator():
 		self.rootTest = 'tests'
 		self.author='\t{\n\t\t\t"name": "Enrique Nieto",\n\t\t\t"email": "ennima@hotmail.com"\n\t\t}'
 
+	
+
+	def emptyGen(self, pname, composerJson):
+		default_path = self.projectPath+pname
+		if not os.path.exists(default_path):
+		    os.makedirs(default_path)
+		    os.makedirs(default_path+"\\"+self.rootSrc)
+		    if(self.rootTest == ""):
+		    	print("No test")
+		    else:
+		    	os.makedirs(default_path+"\\"+self.rootTest)
+
+		    comp_file = open(default_path+"\\composer.json","+w")
+		    comp_file.write(composerJson)
+		    comp_file.close()
+		    print(os.getcwd())
+		    os.chdir(default_path)
+		    print(os.getcwd())
+		    os.system("composer install")
+		    return True
+		else:
+			print("proy exists")
+			return False
+
+	def testDrivenGen(self, pname, composerJson):
+		default_path = self.projectPath+pname
+
+		if(self.emptyGen(pname, composerJson)):
+			os.system("composer require phpunit/phpunit --dev")
+			return True
+		else:
+			return False
+		
 
 	def generateEmptyProject(self):
 		composer_proy = "{\n\t\"name\":\"" + self.projectName + "\",\n\t\"description\":\"" + self.projectDescription + "\",\n\t"
 		composer_proy += "\"type\":\"" + self.projectType + "\",\n\t\"license\":\"" + self.license + "\",\n\t" + "\"authors\": [\n\t" + self.author + "\n\t],\n\t\"require\":{},\n\t"
 		composer_proy += '\"autoload\":{\n\t\t\t\t"psr-4":{\n\t\t\t\t\t"' + self.namespace + '\\\\":"' + self.rootSrc + '"\n\t\t\t\t}\n\t}\n}\n'
-		print(composer_proy)
+		
+		return composer_proy
+
+	def makeEmptyProject(self):
+		composerJson = self.generateEmptyProject()
+
+		user, pname = self.projectName.split('/')
+
+		if not os.path.exists(self.projectPath):
+		    os.makedirs(self.projectPath)
+		    if(self.emptyGen(pname,composerJson)):
+		    	return True
+		else:
+			print("path exists")
+			if(self.emptyGen(pname,composerJson)):
+				return True
+
+		composerJson = self.generateEmptyProject()
+
+		user, pname = self.projectName.split('/')
+		if not os.path.exists(self.projectPath):
+		    os.makedirs(self.projectPath)
+		    if(self.testDrivenGen(pname,composerJson)):
+		    	return True
+		else:
+			print("path exists")
+			if(self.testDrivenGen(pname,composerJson)):
+				return True
+		
+
+
+	def setProjectPath(self, newProjectPath):
+		self.projectPath = newProjectPath
+
+
+	def setProjectName(self, projectName):
+		self.projectName = projectName
+
+
+	def setProjectDescription(self, projectDescription):
+		self.projectDescription = projectDescription
+
+
+	def setProjectType(self, projectType):
+		self.projectType = projectType
+
+
+	def setLicense(self, license):
+		self.license = license
+
+
+	def setNamespace(self, namespace):
+		self.namespace = namespace
+
+
+	def setRootSrc(self, rootSrc):
+		self.rootSrc = rootSrc
+
+
+	def setRootTest(self, rootTest):
+		self.rootTest = rootTest
+
+
+	def setAuthor(self, author):
+		self.author = author
+
+
--- tests/ComposerGeneratorTest.py
+++ tests/ComposerGeneratorTest.py
@@ -12,12 +12,90 @@ class ComposerGeneratorTest(unittest.TestCase):
 	def test_it_should_be_able_to_generate_json_project_default_settings(self):
 		generator = ComposerGenerator()
 		
+		#Default settings in class
+		projectPath = "C:\\Users\\enrique.nieto\\Documents\\develops\\PHP\\"
+		projectName = "ennima/test"
+		projectDescription = "Test for composer"
+		projectType = "project"
+		license = "proprietary"
+		namespace = "Test1"
+		rootSrc = 'src/'
+		rootTest = 'tests'
+		author='\t{\n\t\t\t"name": "Enrique Nieto",\n\t\t\t"email": "ennima@hotmail.com"\n\t\t}'
 
-		jsonReturn = '{\n\t\"name":"ennima/test",\n\t\"description":"Test for composer",\n\t"type":"project",\n\t"license":"proprietary",\n\t"authors": [\n\t\t{\n\t\t\t"name": "Enrique Nieto",\n\t\t\t"email": "ennima@hotmail.com"\n\t\t}\n\t],\n\t"require":{},\n\t"autoload":{\n\t\t\t\t"psr-4":{\n\t\t\t\t\t"Test1\\\\":"src/"\n\t\t\t\t}\n\t}\n}\n'
+		jsonReturn = "{\n\t\"name\":\"" + projectName + "\",\n\t\"description\":\"" + projectDescription + "\",\n\t"
+		jsonReturn += "\"type\":\"" + projectType + "\",\n\t\"license\":\"" + license + "\",\n\t" + "\"authors\": [\n\t" + author + "\n\t],\n\t\"require\":{},\n\t"
+		jsonReturn += '\"autoload\":{\n\t\t\t\t"psr-4":{\n\t\t\t\t\t"' + namespace + '\\\\":"' + rootSrc + '"\n\t\t\t\t}\n\t}\n}\n'
 
-		print("###### \n",jsonReturn)
 		self.assertEqual(generator.generateEmptyProject(),jsonReturn)
 		
+
+	def test_it_should_be_able_to_generate_json_project_new_settings(self):
+		generator = ComposerGenerator()
+
+		generator.setProjectPath("C:\\Path\\")
+		generator.setProjectName("corp/test")
+		generator.setProjectDescription("New test for composer")
+		generator.setProjectType("project")
+		generator.setLicense("MIT")
+		generator.setNamespace("Corp")
+		generator.setRootSrc("code/src/")
+		author='\t{\n\t\t\t"name": "Enrique Nieto Martínez",\n\t\t\t"email": "ennima@hotmail.com"\n\t\t}'
+		generator.setAuthor(author)
+
+		jsonReturn = "{\n\t\"name\":\"" + "corp/test" + "\",\n\t\"description\":\"" + "New test for composer" + "\",\n\t"
+		jsonReturn += "\"type\":\"" + "project" + "\",\n\t\"license\":\"" + "MIT" + "\",\n\t" + "\"authors\": [\n\t" + author + "\n\t],\n\t\"require\":{},\n\t"
+		jsonReturn += '\"autoload\":{\n\t\t\t\t"psr-4":{\n\t\t\t\t\t"' + "Corp" + '\\\\":"' + "code/src/" + '"\n\t\t\t\t}\n\t}\n}\n'
+
+		self.assertEqual(generator.generateEmptyProject(),jsonReturn)
+
+	def test_it_should_be_able_to_generate_json_project_default_settings_empty_no_test(self):
+		generator = ComposerGenerator()
+		generator.setRootTest("")
+		self.assertTrue(generator.makeEmptyProject())
+
+	def test_it_should_be_able_to_generate_json_project_default_settings_empty_no_test_but_exists_project(self):
+		generator = ComposerGenerator()
+		generator.setRootTest("")
+		self.assertFalse(generator.makeEmptyProject())
+
+	
+
+	def test_it_should_be_able_to_generate_json_project_empty_no_test(self):
+		generator = ComposerGenerator()
+		generator.setRootTest("")
+		generator.setProjectName("ennima/testA")
+		generator.setNamespace("Corp")
+		self.assertTrue(generator.makeEmptyProject())
+
+	def test_it_should_be_able_to_generate_json_project_empty_no_test_but_project_exists(self):
+		generator = ComposerGenerator()
+		generator.setRootTest("")
+		generator.setProjectName("ennima/testA")
+		generator.setNamespace("Corp")
+		self.assertFalse(generator.makeEmptyProject())
+
+	
+
+	def test_it_should_be_able_to_generate_json_project_default_settings_test_driven(self):
+		generator = ComposerGenerator()
+		self.assertTrue(generator.makeTestDrivenProject())
+
+	def test_it_should_be_able_to_generate_json_project_default_settings_test_driven_but_project_exists(self):
+		generator = ComposerGenerator()
+		self.assertFalse(generator.makeTestDrivenProject())
+
+	
+
+	def test_it_should_be_able_to_generate_json_project_new_settings_test_driven(self):
+		generator = ComposerGenerator()
+		generator.setProjectPath("C:\\Users\\enrique.nieto\\Documents\\develops\\PHP\\PHP2\\")
+		self.assertTrue(generator.makeTestDrivenProject())
+
+	def test_it_should_be_able_to_generate_json_project_new_settings_test_driven_but_project_exists(self):
+		generator = ComposerGenerator()
+		generator.setProjectPath("C:\\Users\\enrique.nieto\\Documents\\develops\\PHP\\PHP2\\")
+		self.assertFalse(generator.makeTestDrivenProject())
 
 
 if __name__ == '__main__':
