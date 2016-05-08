4dedf9b59f960bd621835a7cd74f57d8f0678f60
enrique nieto  martinez <enrique nieto  martinez>
2016-05-08 04:15:12 -0500

    templater Object

--- comp_gen/templater.py
+++ comp_gen/templater.py
@@ -13,8 +13,19 @@ print (str.replace("{is}", "was", 3))'''
 
 template_name ="templater.txt"
 template = ""
+vars_change = [
+				{"var":"is","val":"was"},
+				{"var":"name","val":"Enrique"},
+				{"var":"verv","val":"Great"},
+			  ] 
 
 with open(template_name, 'r') as f:
 	template = f.read()
 
-print (template.replace("{is}", "was"))
+#print (template.replace("{is}", "was"))
+newTemplate = template
+for change_var in vars_change:
+	newTemplate = newTemplate.replace("{"+change_var["var"]+"}",change_var["val"])
+	print(change_var["var"])
+
+print(newTemplate)
