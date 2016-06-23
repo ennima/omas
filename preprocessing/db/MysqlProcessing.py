42b2ddcf150209ac51d6e3f8c72bae9a413bc2ae
Ennima <ennima@hotmail.com>
2016-06-23 00:40:28 -0500

    mysql Process 2

--- preprocessing/db/MysqlProcessing.py
+++ preprocessing/db/MysqlProcessing.py
@@ -8,10 +8,52 @@ class MysqlProcessing:
 	data = []
 	tables = []
 	tables_total = 0
-
+	types = []
 	def __init__(self):
 		#print("creado")
-		pass
+		VARCHAR(x)
+			Case: user name, email, country, subject, password
+
+		TEXT
+			Case: messages, emails, comments, formatted text, html, code, images, links
+
+		MEDIUMTEXT
+			Case: large json bodies, short to medium length books, csv strings
+
+		LONGTEXT
+			Case: textbooks, programs, years of logs files, harry potter and the goblet of fire, scientific research logging
+		types = [
+					{
+						"class":"string",
+						"min":"1",
+						"max":"255",
+						"type":"VARCHAR"
+					},
+					{
+						"class":"string",
+						"min":"256",
+						"max":"65535",
+						"type":"TEXT"
+					},
+					{
+						"class":"string",
+						"min":"65536",
+						"max":"16777215",
+						"type":"MEDIUMTEXT"
+					},
+					{
+						"class":"string",
+						"min":"16777216",
+						"max":"4294967295",
+						"type":"LONGTEXT"
+					},
+					{
+						"class":"string",
+						"min":"16777216",
+						"max":"4294967295",
+						"type":"LONGTEXT"
+					}
+				]
 	
 	def load(self,db_path):
 		print("Loading db: "+db_path)
@@ -29,6 +71,13 @@ class MysqlProcessing:
 	def create_table(self,name,fields):
 		print("Creando tabla ",name)
 
+		for field in fields:
+			if(len(field) == 2):
+				print ("Normal field simple data")
+			elif( len(field) == 3):
+				print ("Normal field")
+			elif( len(field) == 4):
+				print ("Required field")
 
 	def process(self):
 		for obj in self.data:
@@ -40,10 +89,8 @@ class MysqlProcessing:
 				self.tables.append(obj_name)
 				self.current_table = obj_name
 
-
-
 				for field in obj[obj_name]:
-					print(field['name'],field['type'],field['size'])
+					#print(field['name'],field['type'],field['size'])
 					self.current_fields.append(field)
 					# print("Field Len: ",len(field))
 					# for prop in field:
